21fa706a8c3124f5847cdd6ddf69d244ef78795c
Микола Матіос <mikolka09@gmail.com>
2022-09-13 18:33:39 +0200

    13.09.2022

--- src/main/java/itstep/firstspring/controllers/HomeController.java
+++ src/main/java/itstep/firstspring/controllers/HomeController.java
@@ -1,13 +1,26 @@
 package itstep.firstspring.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
+@Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String home(){
-        return "Hello Word!";
+    private int count = 0;
+    @GetMapping("/") //Маршрут, по которому будет отвечать данный метод контроллера
+    public String home(Model model){//Модель представления данных, которую мне нужно наполнить
+        //это аналог ViewData, ViewBag из asp...
+        count++;
+        //передача аналогична передачи asp
+        model.addAttribute("count", count);
+
+        int[]arr = {3,2,1};
+        model.addAttribute("arr", arr);
+        //return VIEW - имя представления
+        //return "</p>This page opens: "+count+"</p>";
+        return "home";
     }
 }
